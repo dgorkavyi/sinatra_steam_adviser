39afdad5e6cc2879e37d441a8ef7abeb8eed6859
moonshine <moonshine@syneforge.com>
2022-06-13 17:08:30 +0300

    job done

--- app.rb
+++ app.rb
@@ -7,27 +7,29 @@ class SteamApp < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
   helpers Sinatra::SteamApp::Helpers
+  
+  @@invalid_steam_id = false
 
-  get '/game' do
+  get '/' do
     erb :index
   end
 
   get '/suggest_game' do
     # Defaults
     steam_web_api_key = '3495AA76D910EBC50BDD1A18174E75C4'
-    steam_id = '76561198066322255'
+    steam_id = params[:steam_id]
 
     # Set apiKey and get owned games
     Steam.apikey = steam_web_api_key
-    owned_games = Steam::Player.owned_games steam_id
-    # Defaults
-    steam_web_api_key = '3495AA76D910EBC50BDD1A18174E75C4'
-    steam_id = '76561198066322255'
 
-    # Set apiKey and get owned games
-    Steam.apikey = steam_web_api_key
-    owned_games = Steam::Player.owned_games steam_id
-
+    begin
+      owned_games = Steam::Player.owned_games steam_id
+    rescue
+      @@invalid_steam_id = true
+      redirect '/'
+    end
+    
+    @@invalid_steam_id = false
     # Sinatra
 
     @game_data = game_hash_data(owned_games)
